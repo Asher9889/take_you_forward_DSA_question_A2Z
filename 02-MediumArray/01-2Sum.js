d9d3373489d584d33ebf5e7f8508d69fe731db4d
Asher9889 <saursbhkushwaha9889@gmail.com>
2025-05-29 12:39:30 +0530

    better using HashMap and pure math

---
+++ 02-MediumArray/01-2Sum.js
@@ -0,0 +1,42 @@
+let arr = [2,6,5,8,11];
+
+/**
+ * Brute Approach: using two loop check every pair and sum it then compare.
+ */
+
+function twoSumBrute (arr, target){
+    
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i + 1; j < arr.length; j++){
+            if((arr[i] + arr[j]) === target){
+                return ([i,j]);
+            }
+        }
+    }
+    
+    return  false;
+}
+
+// console.log(twoSumBrute(arr,13))
+
+/**
+ * Better: First Hashmap the whole array and then just pure math logic
+ * suppose target = 13;
+ *  x = 13 - elem; is x is present in map just return the index of map.
+ */
+
+function twoSumBetter(arr, target){
+    let m = new Map();
+    for(let i = 0; i < arr.length; i++){
+        m.set(arr[i], i);
+    }
+
+    for( let i = 0; i < arr.length; i++){
+        let x = target - arr[i];
+        if(m.has(x) && m.get(x) !== i){
+            return [i, m.get(x)];
+        }
+    }
+    return false;
+}
+console.log(twoSumBetter(arr, 13))
