ff12b2e56ad0c6da2c5863e9f367a6b0c3617eb3
Asher9889 <saursbhkushwaha9889@gmail.com>
2025-06-12 14:42:12 +0530

    moore's voting algo optimal

--- 02-MediumArray/03-MajorityElem.js
+++ 02-MediumArray/03-MajorityElem.js
@@ -26,19 +26,19 @@ function majorityBrute(arr) {
  */
 
 function majorityBetter(arr) {
-    let n = Math.floor(arr.length/2);
+    let n = Math.floor(arr.length / 2);
 
     let map = new Map();
 
     for (let i = 0; i < arr.length; i++) {
-        const val = val;
+        const val = arr[i];
         if (map.has(val)) {
-            map.set(val, map.get(val + 1));
+            map.set(val, map.get(val) + 1);
         } else {
             map.set(val, 1)
         }
 
-        if(map.get(val > n)){
+        if (map.get(val) > n) {
             return val;
         }
     }
@@ -47,10 +47,41 @@ function majorityBetter(arr) {
 console.log(majorityBetter(arr));
 
 /**
- * Optimal Approach
+ * Optimal Approach: Moore's Voting Algorithm
  */
-arr = [2, 2, 3, 4, 2, 3, 3, 4, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+arr = [2, 5, 8, 2, 3, 4, 2, 3, 4, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4];
 
-function majorityOptimal(arr){
+function majorityOptimal(arr) {
+    let el = null;
+    let count = 0;
 
+    // It will give us a majority elem only
+    for (let i = 0; i < arr.length; i++) {
+        if (count === 0) {
+            el = arr[i];
+            count++
+        }
+        else if (arr[i] === el) {
+            count++;
+        }
+        else {
+            count--;
+        }
+    }
+
+    let count1 = 0
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === el){
+            count1++;
+        }
+    }
+
+    return count1 > Math.floor(arr.length/2) ? el : -1
+
+    
 }
+
+console.log(majorityOptimal([3, 3, 4, 2, 3, 3, 3])); // 3
+console.log(majorityOptimal([1, 2, 3, 4]));          // -1
+console.log(majorityOptimal([2, 2, 1, 1, 1, 2, 2])); // 2
+console.log(majorityOptimal([5, 5, 5, 5, 5]));       // 5
